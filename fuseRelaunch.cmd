eb0fc26582c6b8b0252f4af9930a9ff023d09008
Alex Ding <dingalex@gmail.com>
2015-04-27 08:32:48 +1200

    CS-M152A-lab2

---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/Alex/Documents/CSM152A/Lab2/top_tb_isim_beh.exe" -prj "C:/Users/Alex/Documents/CSM152A/Lab2/top_tb_beh.prj" "work.top_tb" "work.glbl" 
